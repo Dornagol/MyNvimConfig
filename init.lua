2caf598ab4af3b7e2388ddb690a0a27bbaa630cc
Hugo Feufeu <hfeufeu@1D6.42angouleme.fr>
2024-10-12 23:19:11 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.o.expandtab = false  -- Utiliser des tabulations au lieu d'espaces
+vim.o.tabstop = 4        -- Largeur d'une tabulation
+vim.o.shiftwidth = 4     -- Largeur d'une indentation
+vim.o.softtabstop = 4    -- Largeur d'une tabulation lors de l'édition
+
+vim.cmd([[
+inoremap <F1> <C-P>
+]])
