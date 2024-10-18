c3236fd3af490d7cc31c4ab08688c2581ba80d09
Dornagol <feufeuhugo@gmail.com>
2024-10-18 22:15:13 +0200

    update of my theme and some utils (like tranparency)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
-  "Zeioth/neon.nvim",
-  opts = {
-    dim_inactive = false,
-    styles = {
-      comments = { italic = true },
-      keywords = { italic = true },
-    },
-},
+	'AlexvZyl/nordic.nvim',
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require('nordic').load()
+	end
 }
