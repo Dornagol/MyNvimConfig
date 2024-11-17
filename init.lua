c9d1121e4a09a1e393dc7c665943b829571267a4
Dornagol <feufeuhugo@gmail.com>
2024-11-17 17:50:19 +0100

    first commit for new config

--- init.lua
+++ init.lua
@@ -1,38 +1,55 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("oil").setup()
-require("transparent").setup({
-	-- table: default groups
-	groups = {
-		'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-		'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-		'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-		'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
-		'EndOfBuffer',
-	},
-	-- table: additional groups that should be cleared
-	extra_groups = {},
-	-- table: groups you don't want to clear
-	exclude_groups = {},
-	-- function: code to be executed after highlight groups are cleared
-	-- Also the user event "TransparentClear" will be triggered
-	on_clear = function() end,
-})
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
 
-require('cmp').setup({
-	enabled = false, -- Désactiver complètement l'autocomplétion
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+require("oil").setup()
+require("toggleterm").setup{
+  -- Configuration optionnelle ici
+  open_mapping = [[<C-/>]],  -- On assigne <Ctrl+/> pour ouvrir ToggleTerm
+  start_in_insert = true     -- Démarre en mode INSERT
+}
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "lua", "c",  "h" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
 })
-vim.cmd([[colorscheme rose-pine-main]])
-vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
-vim.opt.list = false
+vim.api.nvim_set_keymap('n', '<S-l>', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-h>', ':bprevious<CR>', { noremap = true, silent = true })
 vim.o.expandtab = false -- Utiliser des tabulations au lieu d'espaces
 vim.o.tabstop = 4       -- Largeur d'une tabulation
 vim.o.shiftwidth = 4    -- Largeur d'une indentation
 vim.o.softtabstop = 4   -- Largeur d'une tabulation lors de l'édition
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set('n', '<C-p>', '<Cmd>ToggleTerm<CR>', { silent = true, noremap = true })
-vim.keymap.set('t', '<C-p>', '<Cmd>ToggleTerm<CR>', { silent = true, noremap = true })
-
-vim.cmd([[
-inoremap <F1> <C-P>
-]])
+vim.schedule(function()
+  require "mappings"
+end)
